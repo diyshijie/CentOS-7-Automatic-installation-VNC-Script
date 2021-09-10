c5df438c29a87020aed581fa1ea10c270488031d
diyshijie <90442550+diyshijie@users.noreply.github.com>
2021-09-10 16:12:38 +0800

    Update autovnc.sh

--- autovnc.sh
+++ autovnc.sh
@@ -1,8 +1,6 @@
 yum check-update
 yum groupinstall -y "X Window System"
 yum install -y gnome-classic-session gnome-terminalnautilus-open-terminal control-center liberation-mono-fonts gnome-terminal.x86_64 cjkuni-ukai-fonts perl firefox  tigervnc-server net-tools
-unlink /etc/systemd/system/default.target
-ln -sf /lib/systemd/system/graphical.target/etc/systemd/system/default.target
 echo -e "VNCSERVERS=\"1:root\"" >>  /etc/sysconfig/vncservers
 echo -e  "VNCSERVERARGS[1]=\"-geometry 1280x900\"" >>  /etc/sysconfig/vncservers
 cp /usr/lib/systemd/system/vncserver@.service /usr/lib/systemd/system/vncserver@:1.service
@@ -13,4 +11,3 @@ setenforce 0
 systemctl stop firewalld.service 
 systemctl disable firewalld.service 
 vncpasswd
-
